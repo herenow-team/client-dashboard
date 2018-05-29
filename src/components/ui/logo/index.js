37410e79cac6fcdb41259240b8b0951776051bfb
Rafael Castro <rccastrorafael@gmail.com>
2018-05-29 18:12:02 -0300

    add fake logo (#13)

---
+++ src/components/ui/logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import img from './herenow-logo.png'
+import style from './style.css'
+
+const Logo = () => <img className={style.logo} src={img} alt="herenow" />
+
+export default Logo
