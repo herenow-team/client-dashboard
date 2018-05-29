63dfbcfcac20111a2f2f5f4dcf1325b880b4cdcf
Rafael Castro <rccastorafael@gmail.com>
2018-05-29 18:08:45 -0300

    add fake logo

--- src/containers/user-base-screen/index.js
+++ src/containers/user-base-screen/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import {Container} from 'bulma-ui'
+import Logo from '../../components/ui/logo'
 import style from './style.css'
 
 const Header = () => (
   <header className={style.header}>
     <Link to="/" className={`has-text-success ${style.logo}`}>
-      LOGO GOES HERE
+      <Logo />
     </Link>
   </header>
 )
